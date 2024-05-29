7237c1fdb398f170dc74ec992fc5e01a809e076f
Rieky Rayson <r.rayyysss@gmail.com>
2024-05-29 11:56:39 +0700

    Folder Organized

---
+++ Write your first code using C# (Get started with C#, Part 1)/app/Program.cs
@@ -0,0 +1,93 @@
+﻿// ========== IF ELSE STATEMENT ==========
+// Random random = new Random();
+// int daysUntilExpiration = random.Next(12);
+// int discountPercentage = 0;
+
+// // Your code goes here
+// if (daysUntilExpiration <= 10)
+// {
+//     Console.WriteLine("Your subscription will expire soon. Renew now!");
+// }
+// else if (daysUntilExpiration <= 5)
+// {
+//     discountPercentage = 10;
+//     Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!");
+// }
+// else if (daysUntilExpiration == 1)
+// {
+//     discountPercentage = 20;
+//     Console.WriteLine($"Your subscription expires within a day!\nRenew now and save {discountPercentage}%!");
+// }
+// else if (daysUntilExpiration == 0)
+// {
+//     Console.WriteLine("Your subscription has expired.");
+// }
+
+
+// ========== ARRAY LOOPING STATEMENT ==========
+// string[] array = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+// foreach (string item in array)
+// {
+//     Console.WriteLine(item);
+//     if(item.StartsWith("B"))
+//     {
+//         Console.WriteLine("The name starts with 'B'!");
+//     }
+// }
+
+
+
+// ========== CODE READABILITY ==========
+// Random random = new Random();
+// string[] orderIDs = new string[5];
+// // Loop through each blank orderID
+// for (int i = 0; i < orderIDs.Length; i++)
+// {
+//     // Get a random value that equates to ASCII letters A through E
+//     int prefixValue = random.Next(65, 70);
+//     // Convert the random value into a char, then a string
+//     string prefix = Convert.ToChar(prefixValue).ToString();
+//     // Create a random number, pad with zeroes
+//     string suffix = random.Next(1, 1000).ToString("000");
+//     // Combine the prefix and suffix together, then assign to current OrderID
+//     orderIDs[i] = prefix + suffix;
+// }
+// // Print out each orderID
+// foreach (var orderID in orderIDs)
+// {
+//     Console.WriteLine(orderID);
+// }
+
+// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// int total = roll1 + roll2 + roll3;
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
+//     if ((roll1 == roll2) && (roll2 == roll3)) {
+//         Console.WriteLine("You rolled triples!  +6 bonus to total!");
+//         total += 6; 
+//     } else {
+//         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+//         total += 2;
+//     }
+// }
+
+string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+// Reverse the chars
+Array.Reverse(charMessage);
+int x = 0;
+// count the o's
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+// convert it back to a string
+string new_message = new String(charMessage);
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
